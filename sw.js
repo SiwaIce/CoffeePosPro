08ab847113583904a0e5fc8141589321c8c8af34
SiwaIce <iceget6703@gmail.com>
2026-06-27 09:08:11 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline-first + Cache
    ============================================ */
 
-var CACHE_NAME = 'coffee-pos-v12';
+var CACHE_NAME = 'coffee-pos-v13';
 var urlsToCache = [
   './',
   './index.html',
